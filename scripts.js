cc2dc9efdb3cee703cdce1963a6cc389725ccb28
Pedro Rojas <97629233+ninot18@users.noreply.github.com>
2024-06-15 20:17:45 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -16,7 +16,6 @@ let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
     result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
 }
-console.log(result.substring(0, result.length - 1))
 
 document.addEventListener('DOMContentLoaded', () => {
     const starsContainer = document.createElement('div');
@@ -32,4 +31,45 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     document.body.appendChild(starsContainer);
+});
+
+function changeTittle(text){
+    console.log('click')
+    const txt = document.getElementById(text)
+    switch (text) {
+        case 'when':
+            txt.innerHTML = 'Junio, 29'
+            break
+        case 'where':
+            txt.innerHTML = 'Hotel Dann Carlton Crta. 2N 1-60 Cali Valle'
+            break
+        case 'hour':
+            txt.innerHTML = '18:00 a 3:00'
+            break
+        default:
+            break;
+    }
+}
+
+const where = document.getElementById('where')
+const when = document.getElementById('when')
+const hour = document.getElementById('hour')
+const dress = document.getElementById('dress')
+
+when.addEventListener('click', () => {
+    changeTittle('when');
+})
+
+where.addEventListener('click', () => {
+    changeTittle('where');
+})
+
+hour.addEventListener('click', () => {
+    changeTittle('hour');
+})
+
+const url="dresscode.html"
+
+dress.addEventListener('click', () => {
+    window.location.href = url;
 });
